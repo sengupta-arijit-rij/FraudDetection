3670d26cec58e1ce4e8b633c7418ade683f4ef40
sengupta-arijit-rij <sengupta.arijit.rij@gmail.com>
2023-06-14 06:05:04 +0530

    DataPreProcessing file updated

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+
+
+df=pd.read_csv('/Users/arijitsengupta/Documents/Education/ML AI/DataSets/EDA_Gold_Silver_prices.csv')
+df.head()
